b5003505f5398781782bd936cb4050dcb118d289
Lars Kinkel <larskinkel@sp-byods-145-109-14-33.wireless.uva.nl>
2023-01-10 17:32:32 +0100

    begin gemaakt aan representatie

---
+++ rush.py
@@ -0,0 +1,61 @@
+
+
+class Grid:
+    """
+    Grid object that states the size of the board.
+    """
+    pass
+
+
+class Car:
+    """
+    Car object in the rush hour game, size is 2x1
+    """
+    def __init__(self, type, orientation, col, row):
+
+
+
+
+class Truck:
+    """
+    Truck object in the rush hour game, size is 3x1
+    """
+    def __init__(self, type, orientation, col, row):
+
+
+def load(filename: str):
+    with open(filename, 'r') as file:
+        header = file.readline().split(',')
+        header[-1] = header[-1].strip()
+        print(header)
+
+        cars = []
+        for line in file:
+            splits = line.split(',')
+            splits[-1] = splits[-1].strip()
+            length = splits[4]
+
+            if length == '2':
+                car = Car(splits[0], splits[1], splits[2], splits[3])
+            elif length == '3':
+                truck = Truck(splits[0], splits[1], splits[2], splits[3])
+            print(splits)
+
+
+if __name__ == "__main__":
+    load("Rushhour6x6_1.csv")
+
+
+# def make_knapsack(filename: str) -> Knapsack:
+#     with open(filename, 'r') as file:
+#         header = file.readline()
+#         for line in file:
+#             splits = line.split(',')
+#             if len(splits) > 3:
+#                 element = splits[0].strip()
+#                 points = int(splits[1])
+#                 weight = int(splits[2])
+#                 volume = int(splits[3])
+#                 if element == 'knapsack':
+#                     knapsack = Knapsack(Resources(weight, volume))
+#     return knapsack
