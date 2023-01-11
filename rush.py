09712419e7ab237e3dd852a7cad20e14dc625ba8
Laura Outhuis <outhuislaura@gmail.com>
2023-01-11 09:10:10 +0100

    added info

--- rush.py
+++ rush.py
@@ -3,6 +3,11 @@
 class Grid:
     """
     Grid object that states the size of the board.
+
+    Attributes:
+        car: number of cars on the play board
+        truck: number of trucks on the play board
+        size: size of the board
     """
     pass
 
@@ -10,36 +15,49 @@ class Grid:
 class Car:
     """
     Car object in the rush hour game, size is 2x1
+
+    Attributes:
+        type:
+        orientation: if the car is placed horizontally or vertically
+        column: the column where the front of the car is placed
+        row: the row where the the front of the car is placed
     """
+
     def __init__(self, type, orientation, col, row):
-
-
+        pass
 
 
 class Truck:
     """
     Truck object in the rush hour game, size is 3x1
+
+    Attributes:
+        type:
+        orientation: if the car is placed horizontally or vertically
+        column: the column where the front of the car is placed
+        row: the row where the the front of the car is placed
     """
+
     def __init__(self, type, orientation, col, row):
+        pass
 
+    def load(filename: str):
+        with open(filename, 'r') as file:
+            header = file.readline().split(',')
+            header[-1] = header[-1].strip()
+            print(header)
 
-def load(filename: str):
-    with open(filename, 'r') as file:
-        header = file.readline().split(',')
-        header[-1] = header[-1].strip()
-        print(header)
+            cars = []
+            for line in file:
+                splits = line.split(',')
+                splits[-1] = splits[-1].strip()
+                length = splits[4]
 
-        cars = []
-        for line in file:
-            splits = line.split(',')
-            splits[-1] = splits[-1].strip()
-            length = splits[4]
-
-            if length == '2':
-                car = Car(splits[0], splits[1], splits[2], splits[3])
-            elif length == '3':
-                truck = Truck(splits[0], splits[1], splits[2], splits[3])
-            print(splits)
+                if length == '2':
+                    car = Car(splits[0], splits[1], splits[2], splits[3])
+                elif length == '3':
+                    truck = Truck(splits[0], splits[1], splits[2], splits[3])
+                print(splits)
 
 
 if __name__ == "__main__":
