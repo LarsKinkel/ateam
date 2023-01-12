7be48f75fa159e423c6c1a27da4dc3d09a080503
JioMacaya <119301542+JioMacaya@users.noreply.github.com>
2023-01-12 16:10:58 +0100

    visualiseren van het begin van het spel
    Alleen 1,2 en 3 werkt naar behoren. voor de anderen moeten er meer kleuren toegevoegd worden

--- rush.py
+++ rush.py
@@ -1,4 +1,5 @@
 from vehicle import *
+from visualgrid import *
 from grid import Grid, setupgrid
 import csv
 
@@ -12,6 +13,6 @@ class RushHourGame:
 
 
 if __name__ == "__main__":
-
     filename = int(input("What game do you want to see/solve (1-7)?: "))
-    setupgrid(filename)
+    grid = setupgrid(filename)
+    visualize_grid(grid.grid, grid.dim)
--- visualgrid.py
+++ visualgrid.py
@@ -3,14 +3,14 @@ from matplotlib.colors import ListedColormap, BoundaryNorm
 import numpy as np
 
 # Make a 9x9 grid...
-nrows, ncols = 6,6
-# board = np.zeros(nrows*ncols)
-board = [[ 0,  1,  1,  2,  2,  2],
- [ 0,  3,  3,  5,  4,  4],
- [24, 24,  7,  5,  0,  9],
- [ 6,  6,  7,  8,  8,  9],
- [11,  0, 12,  0, 10, 10],
- [11,  0, 12,  0,  0,  0]]
+# nrows, ncols = 6,6
+# # board = np.zeros(nrows*ncols)
+# board = [[ 0,  1,  1,  2,  2,  2],
+#  [ 0,  3,  3,  5,  4,  4],
+#  [24, 24,  7,  5,  0,  9],
+#  [ 6,  6,  7,  8,  8,  9],
+#  [11,  0, 12,  0, 10, 10],
+#  [11,  0, 12,  0,  0,  0]]
 
 # # Set every other cell to a random number (this would be your data)
 # board[::2] = np.random.random(nrows*ncols // 2 + 1)
@@ -18,26 +18,26 @@ board = [[ 0,  1,  1,  2,  2,  2],
 
 # Reshape things into a 6x6 grid.
 # board = board.reshape((nrows, ncols))
+def visualize_grid(board, dimension):
+    norm = BoundaryNorm(np.unique(board), len(np.unique(board)) - 1)
+    cmap = ListedColormap(['white',
+                            'khaki',
+                            'lightblue',
+                            'lightgreen',
+                            'orange',
+                            'blue',
+                            'purple',
+                            'lime',
+                            'grey',
+                            'crimson',
+                            'yellow',
+                            'brown',
+                            'darkgreen',
+                            'red'])
 
-norm = BoundaryNorm(np.unique(board), len(np.unique(board)) - 1)
-cmap = ListedColormap(['white',
-                        'khaki',
-                        'lightblue',
-                        'lightgreen',
-                        'orange',
-                        'blue',
-                        'purple',
-                        'lime',
-                        'grey',
-                        'crimson',
-                        'yellow',
-                        'brown',
-                        'darkgreen',
-                        'red'])
-
-row_labels = range(1,7)
-col_labels = range(1,7)
-plt.imshow(board, cmap = cmap, norm = norm)
-plt.xticks(range(ncols), col_labels)
-plt.yticks(range(nrows), row_labels)
-plt.show()
+    row_labels = range(1,7)
+    col_labels = range(1,7)
+    plt.imshow(board, cmap = cmap, norm = norm)
+    plt.xticks(range(dimension), col_labels)
+    plt.yticks(range(dimension), row_labels)
+    plt.show()
