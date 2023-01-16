7e1981f235dc123b3ff6c076980ca42a42555e16
JioMacaya <119301542+JioMacaya@users.noreply.github.com>
2023-01-16 15:10:56 +0100

    Output 95% klaar

--- rush.py
+++ rush.py
@@ -13,11 +13,16 @@ class RushHourGame:
 if __name__ == "__main__":
     # filename = int(input("What game do you want to see/solve (1-7)?: "))
     grid = setupgrid(1)
+    print(grid.grid)
     visualize_grid(grid.grid, grid.dim)
     # Move vehicle at coordinates a given amount (depending on vehicle orientation)
-    assert grid.move_vehicle(1, 0, -1) == True
-    assert grid.move_vehicle(0, 2, -1) == False  # Border check
-    assert grid.move_vehicle(1, 1, 1) == False  # Collission
-    grid.move_vehicle(3, 3, -1)
+    # assert grid.move_vehicle(1, 0, -1) == True
+    # assert grid.move_vehicle(0, 2, -1) == False  # Border check
+    # assert grid.move_vehicle(1, 1, 1) == False  # Collission
+    
+    grid.move_vehicle(1,1,-1)
 
-    visualize_grid(grid.grid, grid.dim)
+
+
+
+    
