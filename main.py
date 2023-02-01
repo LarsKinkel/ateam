b4b33a757b727d2bb9c744de605ed79bc4b913f4
Lars Kinkel <larskinkel@MacBook-Air-van-Lars-2.local>
2023-02-01 14:34:25 +0100

    verzalemen resultaten van gisteren

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Code.Classes.grid2 import *
 from Code.Algorithms.randomise import *
 from Code.Algorithms.BFS import *
 from Code.Algorithms.DFS import *
-from Code.Algorithms.BFS_H import *
+from Code.Algorithms.BFS_Heuristics import *
 from heuristics import *
 
 import matplotlib.pyplot as plt
@@ -113,7 +113,7 @@ if __name__ == "__main__":
 
     # ---------------------------- BFS algorithm -------------------------------
     # # choose grid that you want to solve:
-    # grid = setupgrid(2)
+    # grid = setupgrid(3)
     #
     # # solving algorithm
     # algo = BFSalgorithm(grid)
@@ -146,7 +146,7 @@ if __name__ == "__main__":
 
     # ------------------------- BFS 3th heuristic ------------------------------
     # # BFS with fifth heuristic (manhattan distance)
-    # grid = setupgrid(4)
+    # grid = setupgrid(7)
     # goalstate = get_goal_state(copy.deepcopy(grid))
     #
     # # solving algorithm
